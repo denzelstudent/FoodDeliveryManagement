8c5229b0c3ca3baeb8f92f7c2ac062c085149efa
DENZEL SIM YEW KWANG 2404768J <2404768J@student.tp.edu.sg>
2026-01-12 10:43:25 +0800

    scaffolding for reward

--- FoodDeliveryManagement/Data/FoodDeliveryManagementContext.cs
+++ FoodDeliveryManagement/Data/FoodDeliveryManagementContext.cs
@@ -17,5 +17,7 @@ namespace FoodDeliveryManagement.Data
         public DbSet<FoodDeliveryManagement.Domain.Vendor> Vendor { get; set; } = default!;
         public DbSet<FoodDeliveryManagement.Domain.Items> Items { get; set; } = default!;
         public DbSet<FoodDeliveryManagement.Domain.Customer> Customer { get; set; } = default!;
+        public DbSet<FoodDeliveryManagement.Domain.Rider> Rider { get; set; } = default!;
+        public DbSet<FoodDeliveryManagement.Domain.Reward> Reward { get; set; } = default!;
     }
 }
